a9d65526899a35f3e455e2a398a5550d12db4177
Sebastian Madejski <209388@edu.p.lodz.pl>
2018-10-08 13:04:30 +0200

    add GUI layout

--- src/address/MainApp.java
+++ src/address/MainApp.java
@@ -1,6 +1,10 @@
 package address;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -8,9 +12,37 @@ public class MainApp extends Application {
         launch(args);
     }
 
+    private BorderPane loadRootElement() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+        try {
+            BorderPane rootBorderPane = (BorderPane) loader.load();
+            return rootBorderPane;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private AnchorPane loadPersonOverview() {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+        try {
+            AnchorPane rootAnchorPane = (AnchorPane) loader.load();
+            return rootAnchorPane;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
     @Override
     public void start(Stage primaryStage) {
-
+        BorderPane rootBorderPane = loadRootElement();
+        AnchorPane personOverview = loadPersonOverview();
+        rootBorderPane.setCenter(personOverview);
+        primaryStage.setScene(new Scene(rootBorderPane));
+        primaryStage.show();
     }
 }
